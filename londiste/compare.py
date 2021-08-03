75dacf0c3bb08bf5ba0a820c94d53bc7c98589be
Marko Kreen <markokr@gmail.com>
2021-08-03 11:17:01 +0300

    compare: drop pre-8.5 compat

--- londiste/compare.py
+++ londiste/compare.py
@@ -34,18 +34,8 @@ class Comparator(Syncer):
         cols = self.calc_cols(src_curs, src_tbl, dst_curs, dst_tbl)
 
         # get sane query
-        v1 = src_db.server_version
-        v2 = dst_db.server_version
         if self.options.count_only:
             q = "select count(1) as cnt from only _TABLE_"
-        elif v1 < 80300 or v2 < 80300:
-            # 8.2- does not have record to text and text to bit casts, so we need to use a bit of evil hackery
-            calc_md5 = "bit_in(textout('x'||substr(md5(textin(record_out(_COLS_))),1,16)), 0, 64)::bigint"
-            q = "select count(1) as cnt, sum(%s) as chksum from only _TABLE_" % calc_md5
-        elif (v1 < 80400 or v2 < 80400) and v1 != v2:
-            # hashtext changed in 8.4 so we need to use md5 in case there is 8.3 vs 8.4+ comparison
-            calc_md5 = "('x'||substr(md5(_COLS_::text),1,16))::bit(64)::bigint"
-            q = "select count(1) as cnt, sum(%s) as chksum from only _TABLE_" % calc_md5
         else:
             # this way is much faster than the above
             q = "select count(1) as cnt, sum(hashtext(_COLS_::text)::bigint) as chksum from only _TABLE_"
