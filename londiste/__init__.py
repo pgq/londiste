82a1696b4c4dc26416f8ca5dbc9d5affd43289bd
Marko Kreen <markokr@gmail.com>
2023-08-27 15:05:00 +0300

    build: convert to pyproject

--- londiste/__init__.py
+++ londiste/__init__.py
@@ -3,3 +3,5 @@
 
 from __future__ import absolute_import, division, print_function
 
+__version__ = "3.11"
+
--- setup.py
+++ setup.py
@@ -3,36 +3,5 @@
 
 from setuptools import setup
 
-with open("README.rst") as f:
-    ldesc = f.read().strip()
-
-setup(
-    name="londiste",
-    description="Database replication based on PgQ",
-    long_description=ldesc,
-    url="https://github.com/pgq/londiste",
-    license="ISC",
-    version="3.11",
-    maintainer="Marko Kreen",
-    maintainer_email="markokr@gmail.com",
-    packages=["londiste", "londiste.handlers"],
-    install_requires=["pgq", "skytools"],
-    entry_points={
-        "console_scripts": [
-            "londiste = londiste.cli:main",
-        ],
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Environment :: No Input/Output (Daemon)",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: ISC License (ISCL)",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3",
-        "Topic :: Database",
-        "Topic :: System :: Clustering",
-        "Topic :: Utilities",
-    ]
-)
+setup()
 
