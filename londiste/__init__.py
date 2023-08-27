bef9c902beb3564be946527247a0ec2580d04004
Marko Kreen <markokr@gmail.com>
2023-08-27 16:03:05 +0300

    style: remove obsolete __future__ use

--- londiste/__init__.py
+++ londiste/__init__.py
@@ -1,7 +1,5 @@
 
 """Replication on top of PgQ."""
 
-from __future__ import absolute_import, division, print_function
-
 __version__ = "3.11"
 
--- londiste/handlers/__init__.py
+++ londiste/handlers/__init__.py
@@ -1,8 +1,6 @@
 """Handlers module
 """
 
-from __future__ import absolute_import, division, print_function
-
 import functools
 import sys
 
